21d06555f118663c5745a50c4cd78a6756ec531f
Ivan Alexander Rodriguez Romero <ivanrr1991@gmail.com>
2024-10-13 10:43:06 -0500

    splash

--- app/src/main/java/com/ivanacademy/education/MainActivity.java
+++ app/src/main/java/com/ivanacademy/education/MainActivity.java
@@ -1,26 +1,31 @@
 package com.ivanacademy.education;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Cargar la librería nativa en lib.cpp
-    static {
-        System.loadLibrary("lib");
-    }
+    private static int SPLASH_TIME_OUT = 2000; // 2 segundos de retardo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash); // Enlazar con el diseño XML del Splash
 
-        // Usar el método de lib.cpp
-        TextView textView = findViewById(R.id.text_view);
-        textView.setText(stringFromJNI());
+        // Ejecutar el cambio de Activity después del tiempo de retardo
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Inicia la pantalla de inicio
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                // Aplica la animación personalizada
+                overridePendingTransition(R.anim.grow_in, R.anim.fade_out);  // Animaciones de entrada y salida
+                finish(); // Finalizar el Splash Activity para evitar regresar a él
+            }
+        }, SPLASH_TIME_OUT);
     }
-
-    // Declarar el método que será implementado en C++
-    public native String stringFromJNI();
 }
+
